82b74ce083bef97e43b57fdc5cc8f0f519cfe65a
Alex <admin@smartcalc.org>
2024-03-26 13:31:48 +0100

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Alex on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
